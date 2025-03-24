0d4b0b05be9ac939ac501cf68647d596d25cefaa
Norbu <Tenzin2300@gmail.com>
2025-03-24 22:39:57 +0100

    html document af

--- 1.py
+++ 1.py
@@ -1,19 +1,20 @@
 #importeer de library pygame
 import pygame
+import math
 #pygame initialeren
 pygame.init()
 # zet de screen erin met de height en width
 screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Game")
 # maak een player aan
 player = [[100,500,50,50],0]
 #player = pygame.Rect((200,250,50,50))
 
-clock = pygame.time.Clock()
-#game loop
+FPS = pygame.time.Clock()
 run = True
+#game loop en event handeler
 while run:
-    clock.tick(60)
-    #event handeler
+    FPS.tick(60)
     #geef de scherm een kleur
     screen.fill((0,120,250))
     #draw de player in het scherm
@@ -21,12 +22,11 @@ while run:
     #de toetsen die je gebruikt om te bewegen
     key = pygame.key.get_pressed()
     if key[pygame.K_a] == True:
-        player[0][0] -= 2
-    elif key[pygame.K_d] == True:
-        player[0][0] += 2
-    elif key[pygame.K_SPACE] == True and player[0][1] == 500:
-        player[1] = -5
-        
+        player[0][0] -= 5
+    if key[pygame.K_d] == True:
+        player[0][0] += 5
+    if key[pygame.K_SPACE] == True and player[0][1] == 500:
+        player[1] = -5   
     player[0][1] += player[1]
     player[1] += 0.1
     if player[0][1] > 500:
@@ -35,12 +35,8 @@ while run:
     #als je op de x klikt word de window gesloten
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-            
-            
-            
-    
+            run = False    
     #refresh de scherm om de eerdere dingen in te zetten
     pygame.display.update()
-#sluit de window af
+
 pygame.quit()
